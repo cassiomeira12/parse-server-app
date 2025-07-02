e2bb4889358ecc0a3dcb6b3edea0b4b1e3ddb52d
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-07-02 16:32:31 -0300

    refactor: add sos imports

--- cloud/main.js
+++ cloud/main.js
@@ -12,4 +12,10 @@ require('./web_visit_history/web_visit_history');
 require('./security/encrypt/encrypt');
 require('./security/otp/otp');
 
+require('./sos/occurrence');
+require('./sos/safety_contact');
+require('./sos/user_sos_config');
+require('./sos/whatsapp/whatsapp_functions');
+require('./sos/whatsapp/whatsapp_two_factor_authentication');
+
 module.exports.app = require('./app');
