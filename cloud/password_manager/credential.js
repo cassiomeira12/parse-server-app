f223f3746e8d8a064c21529beb8e8ddeac2befc3
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-07-03 11:12:16 -0300

    feat: add credential trigger to add object ACL

---
+++ cloud/password_manager/credential.js
@@ -0,0 +1,15 @@
+Parse.Cloud.beforeSave("Credential", async (request) => {
+  const { original, object, user } = request;
+  
+  if (original === undefined) {
+    var acl = new Parse.ACL();
+    acl.setPublicReadAccess(false);
+    acl.setPublicWriteAccess(false);
+    acl.setReadAccess(user.id, true);
+    acl.setWriteAccess(user.id, true);
+    acl.setRoleReadAccess("Admin", true);
+    acl.setRoleWriteAccess("Admin", true);
+    
+    object.setACL(acl);
+  }
+});
