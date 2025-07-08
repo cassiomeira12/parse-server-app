76f331aa8659f3e4304728fbdb6d97f7ecad1ba5
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-07-08 19:12:43 -0300

    refactor: add new endpoints to download app

--- cloud/app.js
+++ cloud/app.js
@@ -5,4 +5,24 @@ app.get('/hello', (req, res) => {
   res.send("Parse Server Cloud Code");
 });
 
+app.get('/download_android_app', async (req, res) => {
+  const config = await Parse.Config.get({ useMasterKey: true });
+  const url = config.get('app_android_store_url');
+  if (url) {
+    res.redirect(301, url);
+  } else {
+    res.status(404).send('Download url not found');
+  }
+});
+
+app.get('/download_ios_app', async (req, res) => {
+  const config = await Parse.Config.get({ useMasterKey: true });
+  const url = config.get('app_apple_store_url');
+  if (url) {
+    res.redirect(301, url);
+  } else {
+    res.status(404).send('Download url not found');
+  }
+});
+
 module.exports = app;
