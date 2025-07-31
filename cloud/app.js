a69905c698681b3b6dd80154388e7254b686197b
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-07-31 12:48:02 -0300

    refactor: check OTP code validation on all http requests

--- cloud/app.js
+++ cloud/app.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const { validationOTPCode } = require('./security/otp/otp');
+require('dotenv/config');
+
 const app = express();
+
+app.all('*', async (req, res, next)  => {
+  const origin = req.get('origin') || req.headers.origin;
+  const ignoreGetRequest = req.method !== 'GET';
+
+  // se origin for null e Get => ignore
+  // se origin for null e nor for Get => valida
+
+  if (ignoreGetRequest) {
+    const endpointsToIgnore = [
+      '/parse/functions/public-rsa-key',
+      '/parse/functions/dev-otp-code',
+    ];
+    if (endpointsToIgnore.indexOf(req.originalUrl) === -1) {
+      try {
+        await validationOTPCode(req.headers);
+      } catch (error) {
+        return res.status(400).send({'error': error});
+      }
+    }
+  }
+  next();
+});
 
 app.get('/hello', (req, res) => {
   res.send("Parse Server Cloud Code");
