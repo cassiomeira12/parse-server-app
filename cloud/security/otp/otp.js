969afe923672f3dccae41d16070f77eefe248e5c
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-08-29 12:55:37 -0300

    refactor: add decode base64 on dev-otp-code

--- cloud/security/otp/otp.js
+++ cloud/security/otp/otp.js
@@ -1,12 +1,14 @@
 const { authenticator } = require('otplib');
 
 const { encryptData, decryptData } = require('../encrypt/encrypt');
+// const nodeBase64 = require('nodejs-base64-converter');
 
 // Parse.Cloud.define('dev-otp-code', async (request) => {
 //   const config = await Parse.Config.get({ useMasterKey: true });
 //   const secret = config.get('otp_secret');
 //   const code = authenticator.generate(secret);
-//   const publicKey = config.get('rsa_public_key');
+//   const publicKeyBase64 = config.get('rsa_public_key');
+//   const publicKey = nodeBase64.decode(publicKeyBase64);
 //   const encryptedCode = encryptData(code, publicKey);
 //   return encryptedCode;
 // });
