a5ca0b6bc2f0545004688b392f199adf1c705295
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2026-01-09 18:50:18 -0300

    feat: add subscribeTopic, unsubscribeTopic

--- cloud/push_notification/push_notification.js
+++ cloud/push_notification/push_notification.js
@@ -1,5 +1,6 @@
 const { validationAdminRules } = require('../roles/validation_roles');
 const authFirebasePushNotification = require('./auth_firebase_push_notification');
+const axios = require('axios');
 
 Parse.Cloud.define('googleAuthToken', async (request) => {
   const { params } = request;
@@ -24,16 +25,16 @@ Parse.Cloud.define('pushNotification', async (request) => {
   const firebaseProjectId = config.get(`projectId_${GCMSenderId}`);
 
   try {
-    const response = await Parse.Cloud.httpRequest({
-      method: 'POST',
+    const response = await axios({
+      method: 'post',
       url: `https://fcm.googleapis.com/v1/projects/${firebaseProjectId}/messages:send`,
-      headers: {
-        "Authorization": `Bearer ${authToken.access_token}`,
-        "Content-Type": "application/json",
-      },
-      body: {
+      data: {
         'message': params.message,
       },
+      headers: {
+        'Authorization': `Bearer ${authToken.access_token}`,
+        'Content-Type': 'application/json'
+      }
     });
 
     const messageId = response.data.name.replace(`projects/${firebaseProjectId}/messages/`, '');
@@ -42,16 +43,152 @@ Parse.Cloud.define('pushNotification', async (request) => {
       'messageId': messageId,
     };
   } catch (error) {
-    if (error.data.error.status == 'NOT_FOUND') {
-      // APP FOI DESINSTALADO
-      console.log('app UNINSTALLED');
+    if (error.response.status === 404 || error.response === 'Not Found') {
+      throw 'APP_WAS_UNINSTALLED';
     }
-
-    throw error.data.error.status;
+    throw error;
   }
 
 }, validationAdminRules, {
   fields: ['GCMSenderId', 'message'],
+  requireUser: true
+});
+
+Parse.Cloud.define('subscribeTopic', async (request) => {
+  const { params, headers } = request;
+
+  const topic = params.topic;
+
+  const ip = (headers['ip'] ?? request.ip).replace('::ffff:','');
+  const installationId = `${ip} ${request.installationId}`.toLowerCase();
+
+  const queryInstallation = new Parse.Query("_Installation");
+  queryInstallation.equalTo("installationId", installationId);
+  
+  const currentInstallation = await queryInstallation.first({ useMasterKey: true });
+
+  if (currentInstallation === undefined) {
+    throw 'device installation was not founded';
+  }
+
+  const GCMSenderId = currentInstallation.get("GCMSenderId");;
+  const deviceToken = currentInstallation.get("deviceToken");
+
+  if (deviceToken === undefined) {
+    throw 'device has no firebase push token';
+  }
+
+  try {
+    const authToken = await authFirebasePushNotification(GCMSenderId);
+
+    await axios({
+      method: 'post',
+      url: `https://iid.googleapis.com/iid/v1/${deviceToken}/rel/topics/${topic}`,
+      headers: {
+        'Authorization': `Bearer ${authToken.access_token}`,
+        'access_token_auth': 'true'
+      }
+    });
+
+    const topicsResponse = await axios({
+      method: 'get',
+      url: `https://iid.googleapis.com/iid/info/${deviceToken}`,
+      params: {
+        'details': true,
+      },
+      headers: {
+        'Authorization': `Bearer ${authToken.access_token}`,
+        'access_token_auth': 'true'
+      }
+    });
+
+    const data = topicsResponse.data;
+    const topics = data['rel']['topics'];
+
+    const channels = [];
+
+    Object.keys(topics).forEach(function(key, index) {
+      channels.push(key);
+    });
+
+    currentInstallation.set('channels', channels);
+    currentInstallation.save(null, { useMasterKey: true });
+
+    return channels;
+  } catch (error) {
+    throw error;
+  }
+}, {
+  fields: ['topic'],
+  requireUser: true
+});
+
+Parse.Cloud.define('unsubscribeTopic', async (request) => {
+  const { params, headers } = request;
+
+  const topic = params.topic;
+
+  const ip = (headers['ip'] ?? request.ip).replace('::ffff:','');
+  const installationId = `${ip} ${request.installationId}`.toLowerCase();
+
+  const queryInstallation = new Parse.Query("_Installation");
+  queryInstallation.equalTo("installationId", installationId);
+  
+  const currentInstallation = await queryInstallation.first({ useMasterKey: true });
+
+  if (currentInstallation === undefined) {
+    throw 'device installation was not founded';
+  }
+
+  const GCMSenderId = currentInstallation.get("GCMSenderId");;
+  const deviceToken = currentInstallation.get("deviceToken");
+
+  if (deviceToken === undefined) {
+    throw 'device has no firebase push token';
+  }
+
+  try {
+    const authToken = await authFirebasePushNotification(GCMSenderId);
+
+    await axios({
+      method: 'delete',
+      url: `https://iid.googleapis.com/iid/v1/${deviceToken}/rel/topics/${topic}`,
+      headers: {
+        'Authorization': `Bearer ${authToken.access_token}`,
+        'access_token_auth': 'true'
+      }
+    });
+
+    const topicsResponse = await axios({
+      method: 'get',
+      url: `https://iid.googleapis.com/iid/info/${deviceToken}`,
+      params: {
+        'details': true,
+      },
+      headers: {
+        'Authorization': `Bearer ${authToken.access_token}`,
+        'access_token_auth': 'true'
+      }
+    });
+
+    const data = topicsResponse.data;
+    const topics = data['rel']['topics'];
+
+    const channels = [];
+
+    Object.keys(topics).forEach(function(key, index) {
+      channels.push(key);
+    });
+
+    currentInstallation.set('channels', channels);
+    currentInstallation.save(null, { useMasterKey: true });
+
+    return channels;
+  } catch (error) {
+    throw error;
+  }
+}, {
+  fields: ['topic'],
   requireUser: true
 });
 
