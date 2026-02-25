71507690d4cacff0272d77861a45775a69d938da
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2026-02-25 17:13:35 -0300

    refactor: remove function web-visit-history

--- cloud/web_visit_history/web_visit_history.js
+++ cloud/web_visit_history/web_visit_history.js
@@ -1,35 +1,15 @@
-const { validationAdminRules } = require('../roles/validation_roles');
+Parse.Cloud.beforeFind('WebsiteVisitHistory', (req) => {
+  let query = req.query;
+  query.descending('createdAt');
+});
 
-Parse.Cloud.define('web-visit-history', async (request) => {
-  const { user } = request;
-
-  const query = new Parse.Query("WebsiteVisitHistory");
-
-  const response = await query.findAll({ sessionToken: user.getSessionToken() });
-
-  return response.map((item) => {
-    return {
-      objectId: item.id,
-      website: item.get("website") ?? null,
-      ip: item.get("ip") ?? null,
-      userAgent: item.get("userAgent") ?? null,
-      country: item.get("country") ?? null,
-      countryCode: item.get("countryCode") ?? null,
-      countryFlag: item.get("countryCode") == null ? null : `https://flagcdn.com/w320/${item.get("countryCode").toLowerCase()}.png`,
-      region: item.get("region") ?? null,
-      regionName: item.get("regionName") ?? null,
-      city: item.get("city") ?? null,
-      zip: item.get("zip") ?? null,
-      lat: item.get("lat") ?? null,
-      lon: item.get("lon") ?? null,
-      timezone: item.get("timezone") ?? null,
-      isp: item.get("isp") ?? null,
-      org: item.get("org") ?? null,
-      as: item.get("as") ?? null,
-      createdAt: item.createdAt.toISOString(),
-      updatedAt: item.updatedAt.toISOString(),
-    };
+Parse.Cloud.afterFind('WebsiteVisitHistory', async (request) => {
+  request.objects.map((object) => {
+    object.unset('ACL');
+    const countryCode = object.get('countryCode');
+    if (countryCode != undefined) {
+      const flag = `https://flagcdn.com/w320/${countryCode.toLowerCase()}.png`;
+      object.set('countryFlag', flag);
+    }
   });
-}, validationAdminRules, {
-  requireUser: true
 });
